a5b1efef20fa37ba7501d5313bbd7de65652da1e
cessy <cessycode@gmail.com>
2020-09-11 19:12:12 -0400

    upload updated files

---
+++ precache-manifest.2e9fb2bdc840a4e56848a4ed1d4f25cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50c4e9f0bbb0fd9b0620beb12366259",
+    "url": "/roboFindApp/index.html"
+  },
+  {
+    "revision": "c4b4d4536c8fde0ab725",
+    "url": "/roboFindApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "fb6e9606b0ded6ab5ca1",
+    "url": "/roboFindApp/static/css/main.8311d133.chunk.css"
+  },
+  {
+    "revision": "c4b4d4536c8fde0ab725",
+    "url": "/roboFindApp/static/js/2.82874ea2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roboFindApp/static/js/2.82874ea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6e9606b0ded6ab5ca1",
+    "url": "/roboFindApp/static/js/main.e13cd3aa.chunk.js"
+  },
+  {
+    "revision": "69f43ab18e0191f3baa7",
+    "url": "/roboFindApp/static/js/runtime-main.2238aa64.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/roboFindApp/static/media/SEGA.82e09f78.TTF"
+  }
+]);
